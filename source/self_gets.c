f8ad1bb48f48e4c0b4e61f5284e50fd6c14ed795
KwinnerChen <chenkai.c.k@hotmail.com>
2020-05-05 02:23:31 +0800

    update:使用标准库函数取代多次迭代

--- source/self_gets.c
+++ source/self_gets.c
@@ -1,25 +1,23 @@
 // s_gets.c -- 自定义输入源码
 
 #include <stdio.h>
+#include <string.h>
 #include "self_gets.h"
 
 char * s_gets(char * st, int n)
 {
     char * ret_val;
-    int i = 0;
+    char * find;
     
     ret_val = fgets(st, n, stdin);                // 由stdin读取n-1个字符，合并空字符赋值给数组st。返回该数组地址
 
     if (ret_val)
     {
-        while (st[i] != '\n' && st[i] != '\0')    // 检查字符读取字符是否完整（有无换行符），且最终字符索引指向换行符或空字符位置
+        find = strchr(st, '\n');    // 检查字符读取字符是否完整（有无换行符），且最终字符索引指向换行符或空字符位置
+          
+        if (find)                        // 不保留换行符
         {
-            i++;
-        }
-
-        if (st[i] == '\n')                        // 不保留换行符
-        {
-            st[i] = '\0';
+            *find = '\0';
         }
         else
         {
